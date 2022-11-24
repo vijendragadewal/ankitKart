0c579e92fd47b16984ea2abae9d0afd73a83ee50
Ankit Gupta <agupta@technomatz.com>
2022-11-24 18:42:27 +0530

    Add theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import Dashboard from "./Component/Dashbord";
 import { Login } from "./Component/Login";
 import { Routes, Route } from "react-router-dom";
+import Home from "./Component/Home";
+
 function App() {
   return (
     <Routes>
-      <Route exact path="/" element={<Dashboard />} />
+      <Route exact path="/" element={<Home />} />
       <Route exact path="/login" element={<Login />} />
     </Routes>
   );
--- src/Component/Dashbord.jsx
+++ src/Component/Dashbord.jsx
@@ -4,38 +4,53 @@ import { BaseURL } from "../config";
 const Dashboard = () => {
   const [products, setProducts] = useState([]);
   useEffect(() => {
-    fetch(BaseURL + 'products')
-      .then(result => result.json())
-      .then(data => setProducts(data))
+    fetch(BaseURL + "products")
+      .then((result) => result.json())
+      .then((data) => setProducts(data));
   }, []);
-  console.log(products)
-  return (<div>
-    <h1>
-      Welcome Ankit
-    </h1>
-    <div style={{ display: 'flex', flexWrap: 'wrap', width: '100%', gap: '20px' }}>
-      {products && products.map(item => {
-        return (
-          <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: '400px',
-            border: '1px solid',
-            padding: '10px',
-            borderRadius: '24px'
-          }}>
-            <img src={item.image} alt={item.title} width={'100px'} height={'100px'} />
-            <p>{item.title}</p>
-            <p>{item.price}</p>
-            <p>{item.category}</p>
-            <p>{item.description}</p>
-          </div>
-        )
-      })}
+  console.log(products);
+  return (
+    <div>
+      <h1>Welcome Ankit</h1>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          width: "100%",
+          gap: "20px",
+        }}
+      >
+        {products &&
+          products.map((item) => {
+            return (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "400px",
+                  border: "1px solid",
+                  padding: "10px",
+                  borderRadius: "24px",
+                }}
+              >
+                <img
+                  src={item.image}
+                  alt={item.title}
+                  width={"100px"}
+                  height={"100px"}
+                />
+                <p>{item.title}</p>
+                <p>{item.price}</p>
+                <p>{item.category}</p>
+                <p>{item.description}</p>
+              </div>
+            );
+          })}
+      </div>
     </div>
-  </div>);
-}
+  );
+};
 
 export default Dashboard;
